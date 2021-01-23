031f450aa1d5fc52f38dcea9d21fe2252bd1442d
Andrew D'Agostino <adagos04@uoguelph.ca>
2021-01-23 16:39:39 -0500

    Add search method for course level/number

--- classes/descriptr_searches.py
+++ classes/descriptr_searches.py
@@ -61,3 +61,61 @@ class DescSearches:
                 returnCourses.append(course)
 
         return returnCourses
+
+    def byCourseLevel(self, courses, level):
+        """
+        Filter the passed array of courses by passed course level
+
+        Args:
+            courses (List<Course>): An array of Course data structures.
+            level (Integer): The leading digit of the course number
+
+        Raises:
+            ValueError (Exception): If level is not an integer between 1 and 9
+
+        Returns:
+            (list): A list of courses with course numbers starting with the passed digit
+        """
+
+        returnCourses = []
+
+        if not isinstance(level, Integer):
+            raise ValueError("Course level must be an integer")
+
+        if level < 1 or level > 9:
+            raise ValueError("Course level has invalid range")
+
+        for course in courses:
+            if str(course.number).startsWith(str(level)):
+                returnCourses.append(course)
+
+        return returnCourses
+
+    def byCourseNumber(self, courses, number):
+        """
+        Filter the passed array of courses by passed course number (eg. 1250, 4720).
+
+        Args:
+            courses (List<Course>): An array of Course data structures.
+            number (Integer): The 4-digit number of a course
+
+        Raises:
+            ValueError (Exception): If number is not a 4-digit integer
+
+        Returns:
+            (list): A list of courses with matching course numbers
+        """
+
+        returnCourses = []
+
+        if not isinstance(number, Integer):
+            raise ValueError("Course number must be an integer")
+
+        if number < 1000 or number > 9999:
+            raise ValueError("Course number has invalid range")
+
+        for course in courses:
+            if course.number == number:
+                returnCourses.append(course)
+
+        return returnCourses
