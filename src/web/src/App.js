4b0e5529bdda1f24a4e1ddce69781fe73bdf243d
Andrew D'Agostino <adagos04@uoguelph.ca>
2021-03-03 14:00:40 -0500

    Update to use bootstrap table

--- src/web/src/App.js
+++ src/web/src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import CourseTable from './components/CourseTable';
 import Search from './components/Search';
 
-function App() {
-    const mockCourses = [{
-      code: "cis",
-      number: 2750,
-      name: "Software Systems Development and Integration",
-      description: "This course introduces techniques and tools used in the development of large \
-        software systems. Students learn methods for organizing and constructing modular systems,\
-        manipulating files, introductory interface design, and use of databases. Software tools \
-        for managing projects, database connectivity, configuration management, and system \
-        application programmer interfaces are also covered.",
-      credits: "0.5"},
-      {'group': 'Computing and Information Science', 'departments': ['School of Computer Science'], 'code': 'CIS', 'number': '1050', 'name': 'Web Design and Development', 'semesters_offered': ['S', 'W'], 'lecture_hours': 0.0, 'lab_hours': 0.0, 'credits': 0.5, 'description': 'An introduction to the basics of designing and developing a website. It examines the basic concepts, technologies, issues and techniques required to develop and maintain websites. The course is suitable for students with no previous programming experience.', 'distance_education': 'Offered through Distance Education format only.', 'prerequisites': [], 'equates': [], 'corequisites': [], 'restrictions': [], 'capacity_available': 3, 'capacity_max': 160, 'is_full': false}
-    ];
-  return (
-    <div className="App" style={{padding: "100px"}}>
-      <Search/>
-      <CourseTable courses={mockCourses}/>
-    </div>
-  );
-}
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default App;
+        this.state = {
+            courses: [],
+        };
+
+        this.updateCourses = this.updateCourses.bind(this);
+    }
+
+    updateCourses = (courses) => this.setState({ courses: courses });
+
+    render() {
+        return (
+            <div className='App' style={{ padding: '100px' }}>
+                <Search updateCourses={this.updateCourses} />
+                <CourseTable courses={this.state.courses} />
+            </div>
+        );
+    }
+}
--- src/web/src/components/CourseTable.js
+++ src/web/src/components/CourseTable.js
@@ -1,37 +1,70 @@
-/*
+/**
  * CourseTable.js
+ *
+ * Accepts a list of courses and displays some information of the Course JSON data in a table.
+ *
+ * Props:
+ *  - courses (list<object>): The courses to display.
  */
 
-function CourseTable({courses}) {
-  /* Accepts a list of courses and displays some information of the Course JSON data in a table.
+import { Table } from 'react-bootstrap';
+import CourseTableRow from './CourseTableRow';
 
-    Props:
-      - courses (list<object>): The courses to display.
-          Uses fields code, number, name, description, and credits.
-  */
-  const rows = courses.map((course) =>
-    <tr key={course.code + course.number.toString()}>
-      <th scope="row">{`${course.code.toUpperCase()}*${course.number}`}</th>
-      <td>{course.name}</td>
-      <td>{course.description}</td>
-      <td>{course.credits}</td>
-    </tr> 
-  );
-  return (
-    <table className="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Course</th>
-          <th scope="col">Name</th>
-          <th scope="col">Description</th>
-          <th scope="col">Credit Weight</th>
-        </tr>
-      </thead>
-      <tbody>
-        {rows}
-      </tbody>
-    </table>
-  );
+export default function CourseTable(props) {
+    let courses = props.courses;
+
+    // TODO: REMOVE DUMMY DATA ONCE SEARCH WORKING
+    courses = [
+        {
+            code: 'cis',
+            number: 2750,
+            name: 'Software Systems Development and Integration',
+            description:
+                'This course introduces techniques and tools used in the development of large \
+          software systems. Students learn methods for organizing and constructing modular systems,\
+          manipulating files, introductory interface design, and use of databases. Software tools \
+          for managing projects, database connectivity, configuration management, and system \
+          application programmer interfaces are also covered.',
+            credits: '0.5',
+        },
+        {
+            group: 'Computing and Information Science',
+            departments: ['School of Computer Science'],
+            code: 'CIS',
+            number: '1050',
+            name: 'Web Design and Development',
+            semesters_offered: ['S', 'W'],
+            lecture_hours: 0.0,
+            lab_hours: 0.0,
+            credits: 0.5,
+            description:
+                'An introduction to the basics of designing and developing a website. It examines the basic concepts, technologies, issues and techniques required to develop and maintain websites. The course is suitable for students with no previous programming experience.',
+            distance_education: 'Offered through Distance Education format only.',
+            prerequisites: [],
+            equates: [],
+            corequisites: [],
+            restrictions: [],
+            capacity_available: 3,
+            capacity_max: 160,
+            is_full: false,
+        },
+    ];
+
+    return (
+        <Table bordered hover responsive>
+            <thead className='thead-dark text-nowrap'>
+                <tr>
+                    <th>Course</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Credit Weight</th>
+                </tr>
+            </thead>
+            <tbody>
+                {courses.map((course) => (
+                    <CourseTableRow course={course} key={course.code + course.number.toString()} />
+                ))}
+            </tbody>
+        </Table>
+    );
 }
-
-export default CourseTable;
