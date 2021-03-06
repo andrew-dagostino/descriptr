aa444187869f625b0dc90a3185cec89d55d9bf47
Nicholas Skoretz <nskoretz@uoguelph.ca>
2021-03-06 13:29:01 -0500

    [#107] Add API tests. Change API to be Flask pkg.
    Add
    - src/api/apipkg/__init__.py
        - "create_app" factory method that initializes Flask.
    - src/api/requirements.txt
        - Dependency on flask-unittest
    - src/api/test/test_api.py
        - Four tests for "/", "/search" endpoints in GET and POST
    
    Change
    - docker-compose-swarm.dev.yml
        - FLASK_APP environment var to point to package.
    - src/api/api.py
        - into the file src/api/apipkg/__init__.py

---
+++ src/api/test/test_api.py
@@ -0,0 +1,40 @@
+"""Test Descriptr JSON API."""
+
+from apipkg import create_app
+import flask_unittest
+import os
+
+
+""" Change working directory to one level above here """
+os.chdir(os.path.dirname(os.path.dirname(__file__)))
+
+
+class TestDescriptrApi(flask_unittest.ClientTestCase):
+    """A unittest class whose "test_*" methods will be called."""
+    app = create_app()
+
+    def test_root(self, client):
+        """Test that the root endpoint returns the correct response."""
+        ret = client.get("/")
+        self.assertIn('available_endpoints', ret.json)
+        self.assertIn('/search', ret.json["available_endpoints"])
+
+    def test_search_get(self, client):
+        """Test that the root endpoint returns the correct response."""
+        ret = client.get("/search")
+        self.assertIn('available_filters', ret.json)
+        self.assertNotEqual(len(ret.json["available_filters"]), 0)
+
+    def test_search_post_simple(self, client):
+        """Test POSTing a simple search and getting correct response."""
+        ret = client.post("/search", json=dict(number=2750))
+        ret_json = ret.get_json()
+        self.assertIn('courses', ret_json)
+        self.assertGreater(len(ret_json["courses"]), 0)
+
+    def test_search_post_complex(self, client):
+        """Test POSTing a complex search and getting correct response."""
+        ret = client.post("/search", json=dict(code="cis", level=1))
+        ret_json = ret.get_json()
+        self.assertIn('courses', ret_json)
+        self.assertGreater(len(ret.json["courses"]), 0)
