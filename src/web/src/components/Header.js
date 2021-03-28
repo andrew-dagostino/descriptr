2089385d5b1e717a641cf13ca066c040ac890b1d
nskoretz <nskoretz@uoguelph.ca>
2021-03-27 23:57:32 -0400

    [#158] Add Electron DL button.
    Add
    ---
    
    - src/web/src/components/DownloadButton.js
        - Dropdown button component.
    - src/web/src/components/Header.js
        - Add DownloadButton to Header and conditional render.
    
    I actually had a hard time finding a definitive way to download a file
    with the Fetch API, this should work. Once we have the backend
    /api/pkg endpoint working this will need to be tested.

--- src/web/src/components/Header.js
+++ src/web/src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
+import DownloadButton from './DownloadButton';
 
 export default class Header extends React.Component {
     constructor(props) {
         super(props);
+
+        // Check if executable or prod web server
+        const isProd = /^file/.test(window.location)
+            || /^https:\/\/cis4250-03\.socs\.uoguelph\.ca/.test(window.location);
         this.state = {
             links: [
                 { link: '/', text: 'Home' },
                 { link: '/help', text: 'Help' },
                 { link: '/about', text: 'About' },
             ],
+            downloadVisible: isProd,
         };
     }
     render() {
@@ -19,19 +26,22 @@ export default class Header extends React.Component {
                 <Navbar.Brand href='/' className='text-white'>
                     Descriptr
                 </Navbar.Brand>
-                <Nav variant='pills' defaultActiveKey='/'>
-                    {this.state.links.map((item) => (
-                        <Nav.Item key={item.text} className='px-2'>
-                            <Nav.Link
-                                to={item.link}
-                                eventKey={item.link}
-                                className='text-white'
-                                as={Link}>
-                                {item.text}
-                            </Nav.Link>
-                        </Nav.Item>
-                    ))}
-                </Nav>
+                <Container fluid>
+                    <Nav variant='pills' defaultActiveKey='/'>
+                        {this.state.links.map((item) => (
+                            <Nav.Item key={item.text} className='px-2'>
+                                <Nav.Link
+                                    to={item.link}
+                                    eventKey={item.link}
+                                    className='text-white'
+                                    as={Link}>
+                                    {item.text}
+                                </Nav.Link>
+                            </Nav.Item>
+                        ))}
+                    </Nav>
+                    {!this.state.downloadVisible && <DownloadButton />}
+                </Container>
             </Navbar>
         );
     }
